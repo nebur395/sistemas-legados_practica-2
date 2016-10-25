7792b345c14a7e0c694e3e95c12c4574c69740d1
Guillermo Robles <guillerobles1995@gmail.com>
2016-10-25 18:44:34 +0200

    Add DAO and VO stub

--- src/main/java/dataAccess/TaskDAO.java
+++ src/main/java/dataAccess/TaskDAO.java
@@ -1,7 +1,41 @@
-package dataAccess;
+package music_sp;
 
-/**
- * Created by ruben on 25/10/16.
- */
+import java.io.IOException;
+import java.util.ArrayList;
+
 public class TaskDAO {
+    public static void newTask(TaskVO t, MusicSp msp) throws IOException {
+        msp.getConnection().writeString("1");
+        doubleEnter(msp);
+        if (t.isGeneral()) {
+            msp.getConnection().writeString("1");
+            doubleEnter(msp);
+        } else {
+            msp.getConnection().writeString("2");
+            doubleEnter(msp);
+        }
+        msp.getConnection().writeString(t.getDate());
+        doubleEnter(msp);
+        if (!t.isGeneral()) {
+            msp.getConnection().writeString(t.getAsignee());
+            doubleEnter(msp);
+        }
+        msp.getConnection().writeString(t.getDescription());
+        doubleEnter(msp);
+        msp.getConnection().writeString("3");
+        doubleEnter(msp);
+    }
+
+    private static void doubleEnter(MusicSp msp) throws IOException {
+        msp.getConnection().enter();
+        //String[] result = msp.getConnection().getScreen();
+        //if (result[41].equals("                                                                                 ")) {
+        //    msp.getConnection().enter();
+        //}
+    }
+
+    public static ArrayList<TaskVO> getTasks(MusicSp msp) {
+        return null;
+    }
+
 }
--- src/main/java/models/TaskVO.java
+++ src/main/java/models/TaskVO.java
@@ -1,7 +1,56 @@
-package models;
+package music_sp;
 
-/**
- * Created by ruben on 25/10/16.
- */
 public class TaskVO {
+    private String description;
+    private String date;
+    private String asignee;
+    // True: general task
+    // False: specific task
+    private boolean type;
+
+    public TaskVO(String description, String date, String asignee) {
+        this.description = description;
+        this.date = date;
+        this.asignee = asignee;
+        this.type = false;
+    }
+
+    public TaskVO(String description, String date) {
+        this.description = description;
+        this.date = date;
+        this.asignee = "";
+        this.type = true;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public String getAsignee() {
+        return asignee;
+    }
+
+    public boolean isGeneral() {
+        return type;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public void setAsignee(String asignee) {
+        this.asignee = asignee;
+    }
+
+    public void setType(boolean type) {
+        this.type = type;
+    }
 }
