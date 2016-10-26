900377a8362a4b46683c8c58d9ba20a4fb845abc
Ruben Moreno Jimeno <nebur395@hotmail.com>
2016-10-27 00:24:35 +0200

    Add app states

---
+++ src/main/webapp/app.js
@@ -0,0 +1,30 @@
+angular.module('musicPsApp', ['ui.router', 'base64'])
+
+    .config(function ($stateProvider, $urlRouterProvider) {
+        $stateProvider
+
+        //starter screen
+            .state('starter', {
+                url: "/starter",
+                templateUrl: "templates/starter.html",
+                controller: "starterCtrl",
+                onEnter: function ($state, auth) {
+                    if (auth.isAuthenticated()) {
+                        $state.go('home');
+                    }
+                }
+            })
+            //home screen
+            .state('home', {
+                url: "/home",
+                templateUrl: "templates/home.html",
+                controller: "homeCtrl",
+                onEnter: function ($state, auth) {
+                    if (!auth.isAuthenticated()) {
+                        $state.go('starter');
+                    }
+                }
+            });
+
+        $urlRouterProvider.otherwise('starter');
+    });
