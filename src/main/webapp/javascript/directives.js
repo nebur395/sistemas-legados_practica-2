9b78f7d02ce341aef5e524cc9b4daad30590d952
Ruben Moreno Jimeno <nebur395@hotmail.com>
2016-10-27 00:28:27 +0200

    Add client's directives

---
+++ src/main/webapp/javascript/directives.js
@@ -0,0 +1,10 @@
+angular.module('musicPsApp')
+
+// include the 'navbar.html' into the <navbar> tag
+    .directive('navbar', function () {
+        return {
+            restrict: 'E',
+            templateUrl: 'templates/components/navbar.html',
+            controller: 'navbarCtrl'
+        }
+    });
