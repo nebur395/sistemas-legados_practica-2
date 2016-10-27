e2869292c6f845904fab06fc01bd4d822e31cec4
Inigo <garomaru95@gmail.com>
2016-10-27 19:55:55 +0200

    Web.xml fixed

--- src/main/java/servlets/Login.java
+++ src/main/java/servlets/Login.java
@@ -4,6 +4,7 @@ import music_sp.MusicSp;
 
 import java.io.IOException;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
