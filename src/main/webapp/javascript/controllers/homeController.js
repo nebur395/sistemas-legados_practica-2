ce5d4248a9f01938675997df556d116fe0970ea4
Ruben Moreno Jimeno <nebur395@hotmail.com>
2016-10-27 14:16:38 +0200

    Add view tasks functionallity

--- src/main/webapp/javascript/controllers/homeController.js
+++ src/main/webapp/javascript/controllers/homeController.js
@@ -1,6 +1,6 @@
 angular.module('musicPsApp')
 
-    .controller('homeCtrl', ['$scope', '$state', 'auth', function ($scope, $state, auth) {
+    .controller('homeCtrl', ['$scope', '$state', 'taskService', function ($scope, $state, taskService) {
 
         // inputs visual variables
         $scope.date = "";
@@ -10,14 +10,49 @@ angular.module('musicPsApp')
         $scope.descriptionMaxLength = false;
         $scope.assigneeMaxLength = false;
 
+        $scope.taskGeneralList = [];    // list of general tasks
+        $scope.taskSpecificList = [];   //list of specific tasks
+
+        $scope.viewAll = true;
+        $scope.viewGeneral = false;
+        $scope.viewSpecific = false;
+
+        // server request for the list of general tasks
+        $scope.getGeneralList = function () {
+            taskService.getGeneralList(function (tasks) {
+                $scope.taskGeneralList = tasks;
+            });
+        };
+        $scope.getGeneralList();
+        sortCustom($scope.taskGeneralList);
+        // server request for the list of specific tasks
+        $scope.getSpecificList = function () {
+            taskService.getSpecificList(function (tasks) {
+                $scope.taskSpecificList = tasks;
+            });
+        };
+        $scope.getSpecificList();
+        sortCustom($scope.taskSpecificList);
+
+        $scope.taskAllList = $scope.taskGeneralList.concat($scope.taskSpecificList);
+        sortCustom($scope.taskAllList);
+
         $scope.filterMode = function (mode) {
             switch (mode) {
                 case 1:        //all filter
+                    $scope.viewAll = true;
+                    $scope.viewGeneral = false;
+                    $scope.viewSpecific = false;
                     break;
                 case 2:        //general filter
+                    $scope.viewAll = false;
+                    $scope.viewGeneral = true;
+                    $scope.viewSpecific = false;
                     break;
                 default:       //specific filter
-                    ;
+                    $scope.viewAll = false;
+                    $scope.viewGeneral = false;
+                    $scope.viewSpecific = true;
             }
         };
 
@@ -52,6 +87,16 @@ angular.module('musicPsApp')
             }
         });
 
+        // sort the [list] array alphabetically by date
+        function sortCustom (list) {
+            list.sort(function(a, b){
+                var x = a.date.toLowerCase();
+                var y = b.date.toLowerCase();
+                if (x < y) {return -1;}
+                if (x > y) {return 1;}
+                return 0;
+            });
+        }
 
         // send the register form to the auth service
         $scope.addTask = function () {
