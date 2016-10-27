d85c60858cc3525bf37683f3511b8494af1160de
Ruben Moreno Jimeno <nebur395@hotmail.com>
2016-10-27 16:24:51 +0200

    Add addTask, logout, getGeneralTask, and getSpecificTask services

--- src/main/webapp/javascript/services.js
+++ src/main/webapp/javascript/services.js
@@ -3,7 +3,7 @@ angular.module('musicPsApp')
 // 'auth' service manage the authentication function of the page with the server
     .factory('auth', function ($state, $http) {
 
-        var _authenticated = false;
+        var _authenticated = true;
 
         return {
             //return true if the user is authenticated
@@ -18,8 +18,14 @@ angular.module('musicPsApp')
 
             //logout function
             logout: function () {
-                auth.authenticate(false);
-                $state.go('starter');
+                $http({
+                    method: 'GET',
+                    url: 'logout'
+                }).success(function (data) {
+                    auth.authenticate(false);
+                    $state.go('starter');
+                }).error(function () {
+                });
             },
 
             //send the login info to the server
@@ -39,6 +45,68 @@ angular.module('musicPsApp')
                 }).error(function (data) {
                     callback(data);
                 });
+            }
+        };
+    })
+
+    // 'exerciseService' service manage the exercise home functions of the page with the server
+    .factory('taskService', function ($state, $http) {
+
+        return {
+            //get the general list
+            getGeneralList: function (callback) {
+                var list = [
+                    {date:"1001",description:"asa",assignee:""},
+                    {date:"0202",description:"bsb",assignee:""},
+                    {date:"3003",description:"csc",assignee:""}
+                ];
+                callback(list);
+
+                /*$http({
+                    method: 'GET',
+                    url: 'viewTasks',
+                    headers: {
+                        "type": "general"
+                    }
+                }).success(function (data) {
+                    callback(data);
+                }).error(function () {
+                });*/
+            },
+
+            //get the general list
+            getSpecificList: function (callback) {
+                var list = [
+                    {date:"0101",description:"aaa",assignee:"a"},
+                    {date:"2002",description:"bbb",assignee:"b"},
+                    {date:"0303",description:"ccc",assignee:"c"}
+                ];
+                callback(list);
+
+                /*$http({
+                    method: 'GET',
+                    url: 'viewTasks',
+                    headers: {
+                        "type": "specific"
+                    }
+                }).success(function (data) {
+                    callback(data);
+                }).error(function () {
+                });*/
+            },
+
+            // add task
+            addTask: function (object) {
+                $http({
+                    method: 'POST',
+                    url: 'newTask',
+                    data: JSON.stringify(object),
+                    headers: {
+                        'Content-Type': 'application/json'
+                    }
+                 }).success(function () {
+                 }).error(function () {
+                 });
             }
         };
     });
